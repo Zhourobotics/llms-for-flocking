83ef8d6f00c70c9ae73578e4f867e4cadb95baa5
vish <38266782+humboldt123@users.noreply.github.com>
2024-01-27 16:10:28 -0500

    fix: fix token bug via summary

--- prompts.py
+++ prompts.py
@@ -1,7 +1,7 @@
 class Flocking:
     agent_role = "You are a drone navigating a two-dimensional space."
     game_description = (
-        "There are other drones in the space, and you are to coordinate with each other to reach a goal position, "
+        "There are other drones in the space, and you must coordinate with each other to reach a goal position, "
         "forming a flock of a specified shape during the process. Keep in mind Boids flocking rules. Your position is: "
         "[{}]. The positions of the other drones are: [{}]. The goal position is [{}]. The maximum velocity is [{}] "
         "units per round. The flock shape is a [{}]. The safe distance between each drone is [{}] units. "
@@ -14,7 +14,7 @@ class Flocking:
         "The flock's final goal position is [{}]. "
         "Keeping in mind your maximum velocity, please select a new position to move to."
     )
-    output_form = (
+    output_format = (
         "Strictly follow the 'Reasoning:..., Position: [x, y]' format to provide your answer; briefly "
         "providing your thought process in the reasoning section while keeping the position section ONLY "
         "for the position you wish to move to this iteration, without any further explanation."
